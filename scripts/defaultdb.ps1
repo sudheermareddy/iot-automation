9ca3e6b596b32731ab8be99d40632bf877945702
ooha-m <omanduri@sysgain.com>
2017-07-24 12:35:00 +0530

    Update defaultdb.ps1

--- scripts/defaultdb.ps1
+++ scripts/defaultdb.ps1
@@ -1,8 +1,8 @@
 param(
 [string] $sqlQueryUrl = "$1",
-[string] $serverName = "$2",
-[string] $sqlUserName = "$3",
-[string] $sqlserverpwd = "$4",
+[string] $sqlservername = "$2",
+[string] $adminUsername = "$3",
+[string] $adminPassword = "$4",
 [string] $piserverscript = "$5",
 [string] $databaseName = "$6"
 )
@@ -10,6 +10,6 @@ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned  -Force
 $client = New-Object System.Net.WebClient
 $client.DownloadFile($sqlQueryUrl,"C:\createdb.sql")
 cd C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\130\Tools\Binn
-sqlcmd.exe -S $serverName -U $sqlUserName -P $sqlserverpwd -i C:\createdb.sql -o "C:\Testoutput.txt"
+sqlcmd.exe -S $sqlservername -U $adminUsername -P $adminPassword -i C:\createdb.sql -o "C:\Testoutput.txt"
 $client.DownloadFile($piserverscript,"C:\piserverscript.sql")
-Invoke-sqlcmd -inputfile "C:\piserverscript.sql" -serverinstance $serverName -U $sqlUserName -P $sqlserverpwd -database $databaseName 
+Invoke-sqlcmd -inputfile "C:\piserverscript.sql" -serverinstance $sqlservername -U $adminUsername -P $adminPassword -database $databaseName 
