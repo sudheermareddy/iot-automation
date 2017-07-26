5f0c819b33792aba0649d27e5aff7fcc2657ed01
ooha-m <omanduri@sysgain.com>
2017-07-26 18:20:46 +0530

    Update simulatorsetup.ps1

--- scripts/simulatorsetup.ps1
+++ scripts/simulatorsetup.ps1
@@ -1,14 +1,17 @@
 param(
 [string] $simulatorUrl = "$1",
-[string] $sqlservername = "$2"
+[string] $sqlservername = "$2",
+[string] $classscheduleurl = "$3",
+[string] $dataserviceappurl = "$4"
 )
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned  -Force
 $client = new-object System.Net.WebClient
 $client.DownloadFile($simulatorUrl,"C:\SimulatorSetup.msi")
-invoke-command -computername $sqlservername -scriptblock {msiexec /qr /i "C:\SimulatorSetup.msi"}
-$piserverconfig = "C:\Program Files (x86)\Default Company Name\SimulatorSetup\PiServerSimulator.exe.config"
-$doc = (Get-Content $piserverconfig) -as [Xml]
-$obj = $doc.configuration.appSettings.add | where {$_.Key -eq 'PiConnectionString'}
-$obj.value = 'Data Source=sqlvm; Initial Catalog=iottestdb; Persist Security Info=True; User ID=sqluser; Password=Sysgain@1234'
-$doc.Save($piserverconfig)
-Start-Process -FilePath " C:\Program Files (x86)\Default Company Name\SimulatorSetup\PiServerSimulator.exe "
+
+#invoke-command -computername $sqlservername -scriptblock {msiexec /qr /i "C:\SimulatorSetup.msi"}
+#$piserverconfig = "C:\Program Files (x86)\Default Company Name\SimulatorSetup\PiServerSimulator.exe.config"
+#$doc = (Get-Content $piserverconfig) -as [Xml]
+#$obj = $doc.configuration.appSettings.add | where {$_.Key -eq 'PiConnectionString'}
+#$obj.value = 'Data Source=sqlvm; Initial Catalog=iottestdb; Persist Security Info=True; User ID=sqluser; Password=Sysgain@1234'
+#$doc.Save($piserverconfig)
+#Start-Process -FilePath " C:\Program Files (x86)\Default Company Name\SimulatorSetup\PiServerSimulator.exe "
