d26ed18fc562f1bb9aedf982008ac16b3481d598
ooha-m <omanduri@sysgain.com>
2017-07-31 12:52:30 +0530

    Update defaultdb.ps1

--- scripts/defaultdb.ps1
+++ scripts/defaultdb.ps1
@@ -8,7 +8,7 @@ param(
 )
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned  -Force
 $client = New-Object System.Net.WebClient
-$client.DownloadFile("https://https://projectiot.blob.core.windows.net/iotp1/ClassSchedule_PiServer1.csv","C:\ClassSchedule_PiServer1.csv")
+$client.DownloadFile("https://projectiot.blob.core.windows.net/iotp2/DataServiceSetup.msi","C:\DataServiceSetup.msi")
 $client.DownloadFile($sqlQueryUrl,"C:\createdb.sql")
 cd C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\130\Tools\Binn
 sqlcmd.exe -S $sqlservername -U $adminUsername -P $adminPassword -i C:\createdb.sql -o "C:\Testoutput.txt"
