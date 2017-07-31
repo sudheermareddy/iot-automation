fe3a207dc79b5379839064540958d7ba7e520ee8
ooha-m <omanduri@sysgain.com>
2017-07-31 13:25:30 +0530

    Update defaultdb.ps1

--- scripts/defaultdb.ps1
+++ scripts/defaultdb.ps1
@@ -12,5 +12,3 @@ $client.DownloadFile("https://projectiot.blob.core.windows.net/iotp2/DataService
 $client.DownloadFile($sqlQueryUrl,"C:\createdb.sql")
 cd C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\130\Tools\Binn
 sqlcmd.exe -S $sqlservername -U $adminUsername -P $adminPassword -i C:\createdb.sql -o "C:\Testoutput.txt"
-$client.DownloadFile($piserverscript,"C:\piserverscript.sql")
-Invoke-sqlcmd -inputfile "C:\piserverscript.sql" -serverinstance $sqlservername -U $adminUsername -P $adminPassword -database $databaseName 
