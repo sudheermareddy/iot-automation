8d42ef95112f1d21157910ded5ed507d23a705f1
ooha-m <omanduri@sysgain.com>
2017-08-01 15:04:59 +0530

    Update simulatorsetup.ps1

--- scripts/simulatorsetup.ps1
+++ scripts/simulatorsetup.ps1
@@ -6,6 +6,7 @@ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned  -Force
 $client = new-object System.Net.WebClient
 $client.DownloadFile($simulatorUrl,"C:\SimulatorSetup.msi")
 C:\SimulatorSetup.msi /qn
+Start-Sleep -s 12
 #$client.DownloadFile("https://projectiot.blob.core.windows.net/iotp2/DataServiceSetup.msi","C:\DataServiceAppSetup.msi")
 #C:\DataServiceSetup.msi /qn
 $piserverconfig = "C:\Program Files (x86)\Default Company Name\SimulatorSetup\PiServerSimulator.exe.config"
